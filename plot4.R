e4a57d73ef0711f870f101e6bec5a53616338506
José <josvil20@hotmail.com>
2015-09-13 18:48:11 -0500

    Correct the third figure in plot 4

--- plot4.R
+++ plot4.R
@@ -9,6 +9,7 @@ subMetering1 <- as.numeric(electric$Sub_metering_1)
 subMetering2 <- as.numeric(electric$Sub_metering_2)
 subMetering3 <- as.numeric(electric$Sub_metering_3)
 
+png("plot4.png", width = 480, height = 480)
 par(mfrow = c(2, 2)) 
 
 plot(electric$DateTime, as.numeric(electric$Global_active_power), type = "l", 
@@ -17,15 +18,14 @@ plot(electric$DateTime, as.numeric(electric$Global_active_power), type = "l",
 plot(electric$DateTime, as.numeric(electric$Voltage), type = "l", 
      xlab = "datetime", ylab = "Voltage")
 
-plot(electric$DateTime, subMetering1, type = "l", ylab = "Energy Submetering", 
+plot(electric$DateTime, subMetering1, type = "l", ylab = "Energy sub metering", 
      xlab="")
 lines(electric$DateTime, subMetering2, type = "l", col="red")
 lines(electric$DateTime, subMetering3, type = "l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       lty = 1, lwd = 2.5, col = c("black", "red", "blue"))
+       lty = 1, lwd = 1, col = c("black", "red", "blue"), bty = "n")
 
 plot(electric$DateTime, as.numeric(electric$Global_reactive_power), type = "l", 
      xlab = "datetime", ylab = "Global_reactive_power")
 
-dev.copy(png, file = "plot4.png", width = 480, height = 480)
 dev.off()
